ad99dbc076ed62e83f6229478b14fb316f656eb5
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-06-11 10:53:31 -0400

    added avatars

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { CameraIcon, PencilIcon } from "@heroicons/react/outline";
+import Avatar from "boring-avatars";
 
 function Header() {
   return (
     <div className="grid grid-cols-3 w-full py-2">
       <div className=" col-span-1 pl-3">
-        <p className="font-semibold">L</p>
+        <Avatar
+          size={40}
+          name="Maria Mitchell"
+          variant="beam"
+          colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+        />
       </div>
       <div className="font-semibold col-span-1 flex items-center justify-center">
         <h1 className="text-xl">Chat</h1>
