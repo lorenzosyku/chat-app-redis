cae2ff683991a222a639126f2751b8bd3b579772
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-07-21 20:30:38 -0400

    fix bug during deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import Sidebar from "../components/Sidebar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
